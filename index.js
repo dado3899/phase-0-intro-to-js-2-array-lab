3574d582db973adaa69ed7291cd9ed0c278b6564
David Doan <daviddoan@Davids-MacBook-Pro.local>
2024-02-21 16:43:47 -0700

    solution

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat(name){
+    const dupeCats = [...cats,name]
+    // dupeCats.push(name)
+    return dupeCats
+}
+function prependCat(name){
+    // const dupeCats = [name,...cats]
+    return [name,...cats]
+}
+function removeLastCat(){
+    const dupeCats = [...cats]
+    dupeCats.pop()
+    return dupeCats
+}
+function removeFirstCat(){
+    const dupeCats = [...cats]
+    dupeCats.shift()
+    return dupeCats
+}
